6982c2e023d0b10080619cb6c4de06197a03ffdb
renendian <2048367631@qq.com>
2023-02-23 09:59:23 +0800

    factory

---
+++ 02-factory-method-patterns/factory.go
@@ -0,0 +1,53 @@
+package main
+
+import "errors"
+
+const (
+	PayWechat = 1
+	PayAli    = 2
+)
+
+type Payment interface {
+	Pay(money float64) error
+}
+
+type Wechat struct {
+	Balance float32
+}
+
+type Ali struct {
+	Balance float32
+}
+
+func (w *Wechat) Pay(money float32) error {
+	if w.Balance < 0 || w.Balance < money {
+		return errors.New("balance not enough")
+	}
+	w.Balance -= money
+	return nil
+}
+
+func (a *Ali) Pay(money float32) error {
+	if a.Balance < 0 || a.Balance < money {
+		return errors.New("balance not enough")
+	}
+	a.Balance -= money
+	return nil
+}
+
+func Pay(payType int, money float32) error {
+	switch payType {
+	case PayWechat:
+		w := &Wechat{
+			Balance: 500,
+		}
+		return w.Pay(100)
+	case PayAli:
+		a := &Ali{
+			Balance: 500,
+		}
+		return a.Pay(100)
+	default:
+		return errors.New("not support pay type")
+	}
+}
---
+++ 02-factory-method-patterns/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Pay(1, 100))
+}
